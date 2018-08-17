663f827182b0290a2dde5f7b60e203cb6894b354
Ryan Pillsbury <ryan@xyo.network>
2018-08-17 11:27:55 -0700

    Implemented Hash Interface with tests

--- src/types/hash-provider.interface.d.ts
+++ src/types/hash-provider.interface.d.ts
@@ -6,12 +6,12 @@ import { IXYOBase } from "xyo-base.interface";
  * @Email:  developer@xyfindables.com
  * @Filename: hash-provider.interface.d.ts
  * @Last modified by: ryanxyo
- * @Last modified time: Friday, 17th August 2018 9:40:16 am
+ * @Last modified time: Friday, 17th August 2018 10:53:14 am
  * @License: All Rights Reserved
  * @Copyright: Copyright XY | The Findables Company
  */
 
 export interface IHashProvider extends IXYOBase {
-  hash(data: Uint8Array): any;
-  verifyHash(data: Uint8Array, hash: Uint8Array): boolean;
+  hash(data: Buffer): Promise<Buffer>;
+  verifyHash(data: Buffer, hash: Buffer): Promise<boolean>;
 }
