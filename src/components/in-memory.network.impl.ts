144f81a0d8c6f660e8b8ee5795daa9cf0a83a0c8
Ryan Pillsbury <ryan@xyo.network>
2018-08-28 15:58:31 -0700

    Documentation Pass on INetwork implementation

--- src/components/in-memory.network.impl.ts
+++ src/components/in-memory.network.impl.ts
@@ -4,16 +4,30 @@
  * @Email:  developer@xyfindables.com
  * @Filename: in-memory.network.impl.ts
  * @Last modified by: ryanxyo
- * @Last modified time: Monday, 20th August 2018 2:59:42 pm
+ * @Last modified time: Tuesday, 28th August 2018 3:57:36 pm
  * @License: All Rights Reserved
  * @Copyright: Copyright XY | The Findables Company
  */
 
 import { INetwork,  IReceiver, IListenerConnection } from '../types/network';
 
+/**
+ * An `InMemoryNetwork` is an in-memory implementation of INetwork.
+ * It provides the hooks for `Receiver` and `Listener` consumer via
+ * an internal message bus
+ */
 export class InMemoryNetwork implements INetwork {
 
+  /** Map of ids to Receivers */
   private readonly receivers: {[s: string]: IReceiver} = {};
+
+  /**
+   * Will register a receiver to listen at the id specified
+   *
+   * @param id The id to listen on
+   * @param receiver The receiver that will handle incoming messages.
+   * @returns Will return true if the receiver can listen at the address, false otherwise
+   */
 
   public registerReceiver(id: string, receiver: IReceiver): boolean {
     if (this.receivers[id]) {
@@ -23,6 +37,14 @@ export class InMemoryNetwork implements INetwork {
     this.receivers[id] = receiver;
     return true;
   }
+
+  /**
+   * Requests a connection from the network to a given id.
+   *
+   * @param id The id to listen on
+   * @returns Will returns an IListenerConnection that can be used to send messages
+   *          to the receivers specified by the `id` parameter
+   */
 
   public async requestConnection(id: string): Promise<IListenerConnection|null> {
     const targetReceiver = this.receivers[id];
