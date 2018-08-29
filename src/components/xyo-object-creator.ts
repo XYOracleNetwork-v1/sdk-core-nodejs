359e915446ee849e733a1e1f680ddcbac883d5cf
Ryan Pillsbury <ryan@xyo.network>
2018-08-28 17:04:53 -0700

    Documentation pass for xyo-object-creator

--- src/components/xyo-object-creator.ts
+++ src/components/xyo-object-creator.ts
@@ -4,7 +4,7 @@
  * @Email:  developer@xyfindables.com
  * @Filename: xyo-object-creator.ts
  * @Last modified by: ryanxyo
- * @Last modified time: Tuesday, 28th August 2018 8:59:14 am
+ * @Last modified time: Tuesday, 28th August 2018 5:00:04 pm
  * @License: All Rights Reserved
  * @Copyright: Copyright XY | The Findables Company
  */
@@ -12,7 +12,18 @@
 import { XyoType } from './xyo-type';
 import { XyoObject } from './xyo-object';
 
+/**
+ * Abstract `XyoObjectCreator`. Provides factory-like services
+ * for initializing objects from an arbitrary set of bytes
+ * following the Major/Minor XYO protocol
+ */
+
 export abstract class XyoObjectCreator extends XyoType {
+
+  /**
+   * Create an XyoObject from a Buffer
+   * @param data A packed byte-stream following the Xyo Major/Minor Protocol
+   */
 
   public static create(data: Buffer): XyoObject | null {
     const major = data.readUInt8(0);
@@ -21,6 +32,13 @@ export abstract class XyoObjectCreator extends XyoType {
 
     return (creator && creator.createFromPacked(data)) || null;
   }
+
+  /**
+   * Locates an XyoObjectCreator if it has been enabled
+   * @param major The major value
+   * @param minor The minor value
+   * @returns Will return the creator if it has been registered. Otherwise it will return `null`
+   */
 
   public static getCreator(major: number, minor: number): XyoObjectCreator | null {
     const minorsMap = XyoObjectCreator.creators[String(major)];
@@ -31,17 +49,42 @@ export abstract class XyoObjectCreator extends XyoType {
     return minorsMap[String(minor)] || null;
   }
 
+  /**
+   * Registers a creator by major/minor
+   * @param major The major value
+   * @param minor The minor value
+   * @param creator A creator
+   */
+
   public static registerCreator(major: number, minor: number, creator: XyoObjectCreator) {
     const minorMap = XyoObjectCreator.creators[String(major)] || {};
     minorMap[String(minor)] = creator;
     XyoObjectCreator.creators[String(major)] = minorMap;
   }
 
+  /** A mapping of creators by their major/minor values */
   private static creators: {[major: string]: {[minor: string]: XyoObjectCreator}} = {};
 
+  /** The default size of the type that is to be created. If it is a dynamic size, null should be returned */
   public abstract defaultSize: number | null;
+
+  /**
+   * For dynamically sized types, this value functions as a size pointer,
+   * corresponding to amount bytes needed to represent the size of the type
+   */
   public abstract sizeOfSize: number | null;
+
+  /**
+   * A creator's primary function is to take packed binary data and return a hydrated
+   * representation of that object
+   * @param params The packed binary data
+   * @returns Should return an `XyoObject` if the data can be unpacked
+   */
   public abstract createFromPacked(params: Buffer): XyoObject;
+
+  /**
+   * Registers the creator for the major and minor values
+   */
 
   public enable() {
     XyoObjectCreator.registerCreator(this.major, this.minor, this);
