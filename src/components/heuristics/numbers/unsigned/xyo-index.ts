0b3b4ddea6611f9fa1d1255f627947b41111b478
Ryan Pillsbury <ryan@xyo.network>
2018-08-30 14:59:20 -0700

    Added unsigned number type XyoIndex

---
+++ src/components/heuristics/numbers/unsigned/xyo-index.ts
@@ -0,0 +1,71 @@
+/*
+ * @Author: XY | The Findables Company <ryanxyo>
+ * @Date:   Thursday, 30th August 2018 1:35:37 pm
+ * @Email:  developer@xyfindables.com
+ * @Filename: xyo-index.ts
+ * @Last modified by: ryanxyo
+ * @Last modified time: Thursday, 30th August 2018 2:57:02 pm
+ * @License: All Rights Reserved
+ * @Copyright: Copyright XY | The Findables Company
+ */
+
+import { XyoNumberUnsigned } from './xyo-number-unsigned';
+import { XyoObjectCreator } from '../../../xyo-object-creator';
+import { XyoResult } from '../../../xyo-result';
+import { XyoNumberType } from '../xyo-number-type';
+
+class XyoIndexCreator extends XyoObjectCreator {
+
+  get major () {
+    return 0x02;
+  }
+
+  get minor () {
+    return 0x04;
+  }
+
+  get sizeOfBytesToGetSize () {
+    return XyoResult.withValue(0);
+  }
+
+  public readSize(buffer: Buffer) {
+    return XyoResult.withValue(4);
+  }
+
+  public createFromPacked(buffer: Buffer) {
+    return XyoResult.withValue(new XyoIndex(buffer.readUInt32BE(0)));
+  }
+}
+
+// tslint:disable-next-line:max-classes-per-file
+export class XyoIndex extends XyoNumberUnsigned {
+
+  public static major() {
+    return XyoIndex.creator.major;
+  }
+
+  public static minor() {
+    return XyoIndex.creator.minor;
+  }
+
+  private static creator = new XyoIndexCreator();
+
+  constructor (public readonly number: number) {
+    super();
+  }
+
+  get size() {
+    return XyoNumberType.INT;
+  }
+
+  get sizeIdentifierSize () {
+    return XyoResult.withValue(null);
+  }
+
+  get id () {
+    return XyoResult.withValue(Buffer.from([
+      XyoIndex.major,
+      XyoIndex.minor
+    ]));
+  }
+}
