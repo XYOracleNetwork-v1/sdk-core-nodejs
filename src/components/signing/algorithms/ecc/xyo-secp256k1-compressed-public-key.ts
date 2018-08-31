e92a28c60b471c26f8c2abec2b05c190449616af
Ryan Pillsbury <ryan@xyo.network>
2018-08-31 10:36:19 -0700

    Ecc types

---
+++ src/components/signing/algorithms/ecc/xyo-secp256k1-compressed-public-key.ts
@@ -0,0 +1,23 @@
+/*
+ * @Author: XY | The Findables Company <ryanxyo>
+ * @Date:   Friday, 31st August 2018 10:29:47 am
+ * @Email:  developer@xyfindables.com
+ * @Filename: xyo-secp256k1-compressed-public-key.ts
+ * @Last modified by: ryanxyo
+ * @Last modified time: Friday, 31st August 2018 10:35:13 am
+ * @License: All Rights Reserved
+ * @Copyright: Copyright XY | The Findables Company
+ */
+
+import { XyoUncompressedEcPublicKey, XyoUncompressedEcPublicKeyCreator } from './xyo-uncompressed-ec-public-key';
+import { XyoResult } from '../../../xyo-result';
+
+// tslint:disable-next-line:max-classes-per-file
+export abstract class XyoSecp256K1CompressedPublicKey extends XyoUncompressedEcPublicKey {
+
+  private static creator = new XyoUncompressedEcPublicKeyCreator(0x01);
+
+  get id() {
+    return XyoResult.withValue(this.rawId);
+  }
+}
