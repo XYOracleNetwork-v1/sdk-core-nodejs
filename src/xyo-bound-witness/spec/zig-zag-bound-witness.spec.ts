c166cd9454f03cd80de1d1813254813e40a632c5
Ryan Pillsbury <ryan@xyo.network>
2018-10-12 09:35:45 -0700

    Moved and updated zig-zag-bound-witness spec

--- src/xyo-bound-witness/spec/zig-zag-bound-witness.spec.ts
+++ src/xyo-bound-witness/spec/zig-zag-bound-witness.spec.ts
@@ -4,15 +4,15 @@
  * @Email:  developer@xyfindables.com
  * @Filename: zig-zag-bound-witness.spec.ts
  * @Last modified by: ryanxyo
- * @Last modified time: Thursday, 11th October 2018 4:48:18 pm
+ * @Last modified time: Friday, 12th October 2018 9:34:39 am
  * @License: All Rights Reserved
  * @Copyright: Copyright XY | The Findables Company
  */
 
-import { XyoZigZagBoundWitness } from '../../xyo-bound-witness/bound-witness/xyo-zig-zag-bound-witness';
+import { XyoZigZagBoundWitness } from '../bound-witness/xyo-zig-zag-bound-witness';
 import { XyoDefaultPackerProvider } from '../../xyo-serialization/xyo-default-packer-provider';
 import { XyoRsaSha256SignerProvider } from '../../xyo-signing/rsa/sha256/xyo-rsa-sha256-signer-provider';
-import { XyoPayload } from '../../xyo-bound-witness/components/payload/xyo-payload';
+import { XyoPayload } from '../components/payload/xyo-payload';
 import { XyoMultiTypeArrayInt } from '../../xyo-core-components/arrays/multi/xyo-multi-type-array-int';
 import { XyoRssi } from '../../xyo-core-components/heuristics/numbers/xyo-rssi';
 import { XyoObject } from '../../xyo-core-components/xyo-object';
@@ -41,13 +41,23 @@ describe(`ZigZagBoundWitness`, () => {
     const aliceBoundWitness = new XyoZigZagBoundWitness(aliceSigners, alicePayload);
 
     const transfer1 = await bobBoundWitness.incomingData(undefined, false);
-    const transfer2 = await aliceBoundWitness.incomingData(transfer1, true);
-    const transfer3 = await bobBoundWitness.incomingData(transfer2, false);
-    const transfer4 = await aliceBoundWitness.incomingData(transfer3, false);
+    assertSerializationDeserializationSpec(transfer1);
 
-    const bobTypedSerialized = bobBoundWitness.serialize(true);
-    const aliceTypedSerialized = aliceBoundWitness.serialize(true);
-    expect(bobTypedSerialized.equals(aliceTypedSerialized)).toBe(true);
+    const transfer2 = await aliceBoundWitness.incomingData(transfer1, true);
+    assertSerializationDeserializationSpec(transfer2);
+
+    const transfer3 = await bobBoundWitness.incomingData(transfer2, false);
+    assertSerializationDeserializationSpec(transfer3);
+
+    const transfer4 = await aliceBoundWitness.incomingData(transfer3, false);
+    assertSerializationDeserializationSpec(transfer4);
+    expect(bobBoundWitness.isEqual(aliceBoundWitness)).toBe(true);
 
   });
 });
+
+function assertSerializationDeserializationSpec(obj: XyoObject) {
+  const obj1Serialized = obj.serialize(true);
+  const obj1Deserialized = XyoObject.deserialize(obj1Serialized);
+  expect(obj.isEqual(obj1Deserialized)).toBe(true);
+}
