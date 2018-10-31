8c0922bea4874e20ce7d9540c03361d52411a656
Ryan <ryanspillsbury90@gmail.com>
2018-10-31 14:17:18 -0700

    Added some process hooks

--- src/xyo-core-components/xyo-logger.ts
+++ src/xyo-core-components/xyo-logger.ts
@@ -1,5 +1,7 @@
 import winston from 'winston';
 
+import DailyRotateFile from "winston-daily-rotate-file";
+
 /**
  * A central logger for the Xyo core
  *
@@ -7,15 +9,22 @@ import winston from 'winston';
  * @class XyoLogger
  */
 export class XyoLogger {
-  private readonly logger = winston.createLogger({
-    level: 'info',
-    format: winston.format.json(),
-    transports: [
-      new winston.transports.Console({
-        format: winston.format.simple()
-      })
-    ]
-  });
+  private readonly logger = (() => {
+    const transport = new DailyRotateFile({
+      filename: 'logs/application-%DATE%.log',
+      datePattern: 'YYYY-MM-DD-HH',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d'
+    });
+
+    return winston.createLogger({
+      transports: [
+        transport,
+        new winston.transports.Console()
+      ]
+    });
+  })();
 
   /** Log to `info` level */
   public info(message: string, ...meta: any[]) {
