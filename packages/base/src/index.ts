f8a1bc4cfc266c0e58f131167a10a23650f3305e
Ryan <ryanspillsbury90@gmail.com>
2018-11-29 17:41:19 -0800

    Added README.md and maintainability badge

---
+++ packages/base/src/index.ts
@@ -0,0 +1,12 @@
+/*
+ * @Author: XY | The Findables Company <ryanxyo>
+ * @Date:   Thursday, 29th November 2018 5:27:14 pm
+ * @Email:  developer@xyfindables.com
+ * @Filename: index.ts
+ * @Last modified by: ryanxyo
+ * @Last modified time: Thursday, 29th November 2018 5:27:28 pm
+ * @License: All Rights Reserved
+ * @Copyright: Copyright XY | The Findables Company
+ */
+
+export { XyoBase } from './xyo-base'
---
+++ packages/logger/src/index.ts
@@ -0,0 +1,12 @@
+/*
+ * @Author: XY | The Findables Company <ryanxyo>
+ * @Date:   Thursday, 29th November 2018 5:26:04 pm
+ * @Email:  developer@xyfindables.com
+ * @Filename: index.ts
+ * @Last modified by: ryanxyo
+ * @Last modified time: Thursday, 29th November 2018 5:26:20 pm
+ * @License: All Rights Reserved
+ * @Copyright: Copyright XY | The Findables Company
+ */
+
+export { XyoLogger } from './xyo-logger'
