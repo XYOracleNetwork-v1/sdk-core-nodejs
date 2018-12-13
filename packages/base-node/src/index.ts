8db8b4e5829ee893a7c9267fbf7b642ab1476b36
Ryan <ryanspillsbury90@gmail.com>
2018-12-13 13:11:27 -0800

    dynamic node port and validation settings

--- packages/base-node/src/index.ts
+++ packages/base-node/src/index.ts
@@ -4,7 +4,7 @@
  * @Email:  developer@xyfindables.com
  * @Filename: index.ts
  * @Last modified by: ryanxyo
- * @Last modified time: Tuesday, 11th December 2018 4:25:32 pm
+ * @Last modified time: Thursday, 13th December 2018 1:10:55 pm
  * @License: All Rights Reserved
  * @Copyright: Copyright XY | The Findables Company
  */
@@ -63,8 +63,12 @@ export class XyoBaseNode extends XyoBase {
 
   protected getNetwork(): IXyoNetworkProvider {
     return this.getOrCreate('IXyoNetworkProvider', () => {
-      return new XyoServerTcpNetwork(11000)
+      return new XyoServerTcpNetwork(this.getNodePort())
     })
+  }
+
+  protected getNodePort(): number {
+    return 11000
   }
 
   protected getCatalogue(): IXyoNetworkProcedureCatalogue {
@@ -206,16 +210,18 @@ export class XyoBaseNode extends XyoBase {
 
   protected getBoundWitnessValidator(): XyoBoundWitnessValidator {
     return this.getOrCreate('XyoBoundWitnessValidator', () => {
-      return new XyoBoundWitnessValidator(
-        {
-          checkPartyLengths: true,
-          checkIndexExists: true,
-          checkCountOfSignaturesMatchPublicKeysCount: true,
-          validateSignatures: true,
-          validateHash: true
-        }
-      )
+      return new XyoBoundWitnessValidator(this.getBoundWitnessValidatorSettings())
     })
+  }
+
+  protected getBoundWitnessValidatorSettings() {
+    return {
+      checkPartyLengths: true,
+      checkIndexExists: true,
+      checkCountOfSignaturesMatchPublicKeysCount: true,
+      validateSignatures: true,
+      validateHash: true
+    }
   }
 
   protected getNestedBoundWitnessExtractor(): XyoNestedBoundWitnessExtractor {
