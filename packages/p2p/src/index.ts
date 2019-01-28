79b0c6e20b51b77d450a1413c9d24025ae4e86b0
Ryan <ryanspillsbury90@gmail.com>
2019-01-28 15:49:23 -0800

    Exposed discovery interface from p2p pacakage

--- packages/p2p/src/index.ts
+++ packages/p2p/src/index.ts
@@ -4,13 +4,13 @@
  * @Email:  developer@xyfindables.com
  * @Filename: index.ts
  * @Last modified by: ryanxyo
- * @Last modified time: Wednesday, 16th January 2019 11:01:39 am
+ * @Last modified time: Monday, 28th January 2019 3:48:55 pm
  * @License: All Rights Reserved
  * @Copyright: Copyright XY | The Findables Company
  */
 
 import "./multiaddr"
-export { IXyoP2PService, IXyoPeer, unsubscribeFn } from './@types'
+export { IXyoP2PService, IXyoPeer, unsubscribeFn, IXyoPeerDiscoveryService } from './@types'
 export { XyoPeerTransport } from './xyo-peer-transport'
 export { XyoPeerDiscoveryService } from './xyo-peer-discovery'
 export { XyoP2PService } from './xyo-p2p-service'
