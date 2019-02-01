bd7afed07e6057d5c805e7dc00399d6aed1acee4
Ryan <ryanspillsbury90@gmail.com>
2019-02-01 15:31:02 -0800

    Added some more utilities

---
+++ scripts/pay-on-delivery.js
@@ -0,0 +1,95 @@
+/*
+ * @Author: XY | The Findables Company <ryanxyo>
+ * @Date:   Friday, 1st February 2019 1:58:31 pm
+ * @Email:  developer@xyfindables.com
+ * @Filename: pay-on-delivery.js
+ * @Last modified by: ryanxyo
+ * @Last modified time: Friday, 1st February 2019 2:41:06 pm
+ * @License: All Rights Reserved
+ * @Copyright: Copyright XY | The Findables Company
+ */
+
+/**
+ * An interactive script to interact with the payondelivery contract
+ */
+
+const { 
+  prompt,
+  shelljs,
+  logInfo,
+  logError,
+  exitWithCode,
+  exitItDockerNotInstalled,
+  getDockerData,
+  restartDockerContainer,
+  destroyContainer,
+  sleepSync,
+  folderExists,
+  fileExists
+} = require('./script-utils')
+
+const Web3 = require('web3')
+const { HttpProvider } = require('web3-providers')
+
+async function main () {
+  if (folderExists('payondelivery')) {
+    const { deleteExistingPayOnDelivery } = await prompt({
+      type: 'confirm',
+      message: 'The `payondelivery` folder already exists, do you want to delete it?',
+      name: 'deleteExistingPayOnDelivery'
+    })
+
+    if (deleteExistingPayOnDelivery) {
+      shelljs.rm('-rf', 'payondelivery')
+      shelljs.exec('git clone https://github.com/XYOracleNetwork/demo-payondelivery-solidity.git payondelivery')
+    } else {
+      if (folderExists('payondelivery/build/contracts/PayOnDelivery.json')) {
+        const networks = await getCurrentPayOnDeliveryDeployments()
+        if (networks) {
+          const deployments = Object.keys(networks)
+          if (deployments.length > 0) {
+            return exitWithCode(1, `PayOnDelivery has already been deployed @ ${networks[deployments[0]].address}`)
+          }
+        }
+      }
+    }
+  } else {
+    logInfo(`Installing payondelivery project`)
+    shelljs.exec('git clone https://github.com/XYOracleNetwork/demo-payondelivery-solidity.git payondelivery')    
+  }
+
+  logInfo(`Building payondelivery project`)
+  shelljs.pushd('payondelivery')
+  shelljs.exec('yarn')
+  shelljs.exec('yarn global add tool-dapploy-nodejs')
+
+  logInfo(`Deploying PayOnDelivery Contract and copying contracts to ifps`)  
+  shelljs.exec('dapploy')
+  shelljs.cp('-r', 'build/contracts ../ipfs/staging')
+  const addToIPFS = shelljs.exec('docker exec ipfs_host ipfs add -r /export/contracts')
+  logInfo(addToIPFS.stdout)
+  shelljs.popd()
+  const networkDeployments = await getCurrentPayOnDeliveryDeployments()
+  if (!networkDeployments) {
+    return exitWithCode(1, 'Something went wrong during deployment of contracts')
+  }
+
+  logInfo(`Current PaymentOnDelivery Contract address ${Object.values(networkDeployments)[0].address}`)
+  exitWithCode(0)
+}
+
+async function getCurrentPayOnDeliveryDeployments() {
+  if (fileExists('payondelivery/build/contracts/PayOnDelivery.json')) {
+    const payOnDeliveryJSON = JSON.parse(shelljs.cat('payondelivery/build/contracts/PayOnDelivery.json').stdout)
+    if (payOnDeliveryJSON.networks) {
+      return payOnDeliveryJSON.networks
+    }
+
+  }
+
+  return undefined
+}
+
+if (require.main === module) {
+  main()
+}
