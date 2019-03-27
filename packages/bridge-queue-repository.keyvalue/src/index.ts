e16bef0234116ff7e7cdab8a415003ad63fc1e65
Carter Harrison <carterjharrison@gmail.com>
2019-03-27 10:37:11 -0700

    remove packages start

--- packages/bridge-queue-repository.keyvalue/src/index.ts
+++
@@ -1,2 +0,0 @@
-
-export { XyoStorageBridgeQueueRepository } from './bridge-queue-repository-storage'
--- packages/bridge.pi/bin/start.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-
-console.log("xyo-pi-bridge start")
-
-const { main } = require('../dist/index.js');
-
-console.log("xyo-pi-bridge imported")
-main()
