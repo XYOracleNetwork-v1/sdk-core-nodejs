2f3cf51a02cf66fc97172b846211c3aeacc5675d
Arie Trouw <arie.trouw@xy.company>
2019-04-17 22:45:18 -0700

    Dependency Cleanup

--- packages/node-network/src/index.ts
+++ packages/node-network/src/index.ts
@@ -3,7 +3,7 @@
  * @Date:   Monday, 28th January 2019 5:15:12 pm
  * @Email:  developer@xyfindables.com
  * @Filename: index.ts
- 
+
  * @Last modified time: Thursday, 28th February 2019 6:34:40 pm
  * @License: All Rights Reserved
  * @Copyright: Copyright XY | The Findables Company
@@ -11,8 +11,6 @@
 
 export {
   IXyoComponentFeatureResponse as IComponentFeatureResponse,
-  IXyoComponentArchivistFeatureDetail,
-  IXyoComponentDivinerFeatureDetail,
   IXyoComponentFeatureDetail,
   IXyoNodeNetwork,
   IXyoComponentFeatureResponse,
