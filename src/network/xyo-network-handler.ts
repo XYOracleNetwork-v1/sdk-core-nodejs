51970df7d545ae264734107a862fb5ffdff484df
Carter Harrison <carterjharrison@gmail.com>
2019-04-23 18:36:23 -0700

    add network handlers

---
+++ src/network/xyo-network-handler.ts
@@ -0,0 +1,30 @@
+import { IXyoNetworkPipe } from './xyo-network-pipe'
+
+export class XyoNetworkHandler {
+
+  private static getSizeEncodedCatalogue (catalogue: Buffer): Buffer {
+    const sizeBuffer = Buffer.alloc(1)
+    sizeBuffer.writeUInt8(catalogue.length, 0)
+    return Buffer.concat([sizeBuffer, sizeBuffer])
+  }
+
+  public pipe: IXyoNetworkPipe
+
+  constructor(pipe: IXyoNetworkPipe) {
+    this.pipe = pipe
+  }
+
+  public sendCataloguePacket (catalogue: Buffer): Promise<Buffer | undefined> {
+    const buffer = XyoNetworkHandler.getSizeEncodedCatalogue(catalogue)
+    return this.pipe.send(buffer, true)
+  }
+
+  public sendChoicePacket (choice: Buffer, response: Buffer): Promise<Buffer | undefined> {
+    const bufferToSend = Buffer.concat([
+      XyoNetworkHandler.getSizeEncodedCatalogue(choice),
+      response
+    ])
+
+    return this.pipe.send(bufferToSend, true)
+  }
+}
---
+++ src/origin/xyo-origin-chain.ts
@@ -0,0 +1,16 @@
+import { IXyoHasher } from '../hashing'
+import { XyoOriginState } from './xyo-origin-state'
+import { IXyoOriginBlockRepository } from '../persist/xyo-origin-block-repository'
+import { IXyoOriginStateRepository } from '../persist/xyo-origin-state-repository'
+
+export class XyoOriginChain {
+  private hasher: IXyoHasher
+  private state: XyoOriginState
+  private blockRepository: IXyoOriginBlockRepository
+
+  constructor(hasher: IXyoHasher, stateRepo: IXyoOriginStateRepository, blockRepo: IXyoOriginBlockRepository) {
+    this.hasher = hasher
+    this.state = new XyoOriginState(stateRepo)
+    this.blockRepository = blockRepo
+  }
+}
