90698212c5c97c2701c5435b80b7fc518c0ba61c
Carter Harrison <carterjharrison@gmail.com>
2019-04-23 18:55:14 -0700

    add zig zag bound witness session

---
+++ src/bound-witness/spec/xyo-zig-zag-bound-witness-session.ts
@@ -0,0 +1,52 @@
+import { XyoZigZagBoundWitness } from '../xyo-zig-zag-bound-witness'
+import { XyoNetworkHandler } from '../../network/xyo-network-handler'
+import { XyoStructure, XyoIterableStructure, XyoBuffer } from '@xyo-network/object-model'
+import { IXyoSigner } from '../../signing/xyo-signer'
+
+export class XyoZigZagBoundWitnessSession extends XyoZigZagBoundWitness {
+  private handler: XyoNetworkHandler
+  private choice: Buffer
+  private cycles = 0
+
+  constructor(handler: XyoNetworkHandler, signedPayload: XyoStructure[], unsignedPayload: XyoStructure[], signers: IXyoSigner[], choice: Buffer) {
+    super(signers, signedPayload, unsignedPayload)
+
+    this.handler = handler
+    this.choice = choice
+  }
+
+  public async doBoundWitness (transfer: XyoIterableStructure | undefined): Promise<void> {
+    if (!this.getIsCompleted()) {
+      const response = await this.sendAndReceive(transfer !== undefined, transfer)
+
+      if (this.cycles === 0 && transfer !== undefined && response !== null) {
+        this.incomingData(response, false)
+      } else {
+        this.cycles++
+        return this.doBoundWitness(response)
+      }
+    }
+  }
+
+  private async sendAndReceive (didHaveData: boolean, transfer: XyoIterableStructure | undefined) {
+    let response: Buffer | undefined
+    const returnData = this.incomingData(transfer, this.cycles === 0 && didHaveData)
+
+    if (this.cycles === 0 && !didHaveData) {
+      response = await this.handler.sendChoicePacket(this.choice, returnData.getContents().getContentsCopy())
+    } else {
+      response = await this.handler.pipe.send(returnData.getContents().getContentsCopy(), this.cycles === 0)
+
+      if (this.cycles === 0 && !response) {
+        throw new Error('Response is null')
+      }
+    }
+
+    if (response) {
+      return new XyoIterableStructure(XyoBuffer.wrapBuffer(response))
+    }
+
+    return undefined
+  }
+
+}
--- src/origin/xyo-origin-chain.ts
+++ src/origin/xyo-origin-chain.ts
@@ -2,6 +2,7 @@ import { IXyoHasher } from '../hashing'
 import { XyoOriginState } from './xyo-origin-state'
 import { IXyoOriginBlockRepository } from '../persist/xyo-origin-block-repository'
 import { IXyoOriginStateRepository } from '../persist/xyo-origin-state-repository'
+import { XyoBoundWitness } from '../bound-witness'
 
 export class XyoOriginChain {
   private hasher: IXyoHasher
@@ -12,5 +13,9 @@ export class XyoOriginChain {
     this.hasher = hasher
     this.state = new XyoOriginState(stateRepo)
     this.blockRepository = blockRepo
+  }
+
+  public async boundWitness (): Promise<XyoBoundWitness | undefined> {
+
   }
 }
