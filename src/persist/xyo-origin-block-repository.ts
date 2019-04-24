422c1992a2f3099dc2f63daa773a75ef7428a9aa
Arie Trouw <arie.trouw@xy.company>
2019-04-24 08:53:15 -0700

    Interface Update

--- src/persist/xyo-origin-block-repository.ts
+++ src/persist/xyo-origin-block-repository.ts
@@ -3,5 +3,5 @@ export interface IXyoOriginBlockRepository {
   removeOriginBlock(hash: Buffer): Promise<void>
   addOriginBlock(hash: Buffer, originBlock: Buffer): Promise<void>
   getOriginBlock(hash: Buffer): Promise<Buffer | undefined>
-  getOriginBlocks(page?: Buffer, limit?: number): Promise<Buffer[]>
+  getOriginBlocks(limit?: number, offset?: Buffer): Promise<Buffer[]>
 }
