0e5d11fa6fbe139553fa5c44f4f6df4b4ebc2d44
Carter Harrison <carterjharrison@gmail.com>
2019-04-23 19:35:00 -0700

    origin chain

--- src/origin/xyo-origin-chain.ts
+++ src/origin/xyo-origin-chain.ts
@@ -3,21 +3,24 @@ import { XyoOriginState } from './xyo-origin-state'
 import { IXyoOriginBlockRepository } from '../persist/xyo-origin-block-repository'
 import { IXyoOriginStateRepository } from '../persist/xyo-origin-state-repository'
 import { XyoBoundWitness } from '../bound-witness'
-import { XyoZigZagBoundWitnessSession } from '../bound-witness/xyo-zig-zag-bound-witness-session'
-import { XyoNetworkHandler } from '../network/xyo-network-handler'
-import { IXyoProcedureCatalogue } from '../network/xyo-procedure-catalogue'
-import { XyoChoicePacket } from '../network/xyo-choice-packet'
-import { XyoIterableStructure, XyoBuffer } from '@xyo-network/object-model'
+import { throwStatement } from '@babel/types'
 
 export class XyoOriginChain {
   private hasher: IXyoHasher
   private state: XyoOriginState
   private blockRepository: IXyoOriginBlockRepository
-  private currentBoundWitnessSession: XyoZigZagBoundWitnessSession | undefined
 
   constructor(hasher: IXyoHasher, stateRepo: IXyoOriginStateRepository, blockRepo: IXyoOriginBlockRepository) {
     this.hasher = hasher
     this.state = new XyoOriginState(stateRepo)
     this.blockRepository = blockRepo
+  }
+
+  public async onBoundWitnessCompleted (boundWitness: XyoBoundWitness) {
+    const hash = boundWitness.getHash(this.hasher)
+    this.state.addOriginBlock(hash)
+
+    await this.state.repo.commit()
+    await this.blockRepository.addOriginBlock(hash.getContents().getContentsCopy(), boundWitness.getContents().getContentsCopy())
   }
 }
