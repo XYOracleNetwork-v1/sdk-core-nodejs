1281684ae615b0674d86e952a8c1574d14bbd600
Carter Harrison <carterjharrison@gmail.com>
2019-04-25 16:47:42 -0700

    add bound witness validator

---
+++ src/bound-witness/xyo-bound-witness-validator.ts
@@ -0,0 +1,45 @@
+import { XyoSignatureVerify } from '../signing/xyo-signer'
+import { XyoBoundWitness } from './xyo-bound-witness'
+
+export class XyoBoundWitnessValidator {
+  private signatureValidators: Map<number, XyoSignatureVerify>
+
+  constructor(signatureValidators: Map<number, XyoSignatureVerify>) {
+    this.signatureValidators = signatureValidators
+  }
+
+  public validate (boundWitness: XyoBoundWitness, allowUnknownSignatures: boolean): boolean {
+    const signingData = boundWitness.getSigningData()
+    const publicKeysParty = boundWitness.getPublicKeys()
+    const signaturesParty = boundWitness.getSignatures()
+
+    if (publicKeysParty.length !== publicKeysParty.length) {
+      return false
+    }
+
+    for (let i = 0; i < publicKeysParty.length; i++) {
+      const publicKeys = publicKeysParty[i]
+      const signatures = signaturesParty[i]
+
+      if (publicKeysParty.length !== signaturesParty.length) {
+        return false
+      }
+
+      for (let i = 0; i < publicKeysParty.length; i++) {
+        const publicKey = publicKeys[i]
+        const signature = signatures[i]
+        const signatureValidator = this.signatureValidators.get(signature.getSchema().id)
+
+        if (signatureValidator) {
+          if (!signatureValidator(publicKey.getAll().getContentsCopy(), signature.getAll().getContentsCopy(), signingData)) {
+            return false
+          }
+        } else if (signatureValidator && !allowUnknownSignatures) {
+          return false
+        }
+      }
+    }
+
+    return true
+  }
+}
