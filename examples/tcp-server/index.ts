17a9014b5f32dc6d94fcd5530751053c038ee1b7
Carter Harrison <carterjharrison@gmail.com>
2019-04-25 17:11:30 -0700

    add archivist procedure catalogue

--- examples/tcp-server/index.ts
+++ examples/tcp-server/index.ts
@@ -1,14 +1,29 @@
-import { XyoServerTcpNetwork, XyoFileOriginStateRepository, XyoMemoryBlockRepository, XyoZigZagBoundWitnessHander, XyoOriginPayloadConstructor, XyoBoundWitnessSuccessListener, XyoOriginState, XyoSha256, IXyoProcedureCatalogue, XyoNetworkHandler, XyoSecp2556k1, XyoGenesisBlockCreator } from '../../dist'
+import { XyoServerTcpNetwork, XyoFileOriginStateRepository, XyoMemoryBlockRepository, XyoZigZagBoundWitnessHander, XyoOriginPayloadConstructor, XyoBoundWitnessInserter, XyoOriginState, XyoSha256, IXyoProcedureCatalogue, XyoNetworkHandler, XyoSecp2556k1, XyoGenesisBlockCreator, XyoCatalogueFlags } from '../../dist'
 
-const testProcedureCatalogue: IXyoProcedureCatalogue = {
+const archivistProcedureCatalogue: IXyoProcedureCatalogue = {
   getEncodedCanDo: () => {
-    return Buffer.from('01', 'hex')
+    return Buffer.from([XyoCatalogueFlags.TAKE_ORIGIN_CHAIN | XyoCatalogueFlags.BOUND_WITNESS])
   },
-  choose: () => {
-    return Buffer.from('01', 'hex')
+  choose: (catalogue: Buffer): Buffer => {
+    if (catalogue.length < 1) {
+      throw new Error('Catalogue must have at least a byte')
+    }
+
+    const catalogueInt = catalogue.readUInt8(0)
+
+    if ((catalogueInt & XyoCatalogueFlags.GIVE_ORIGIN_CHAIN) !== 0) {
+      return new Buffer([XyoCatalogueFlags.TAKE_ORIGIN_CHAIN])
+    }
+
+    return new Buffer([XyoCatalogueFlags.BOUND_WITNESS])
   },
-  canDo: (buffer: Buffer) => {
-    return true
+  canDo: (buffer: Buffer): boolean => {
+    if (buffer.length < 1) {
+      return false
+    }
+
+    const catalogueInt = buffer.readUInt8(0)
+    return (catalogueInt & (XyoCatalogueFlags.GIVE_ORIGIN_CHAIN | XyoCatalogueFlags.BOUND_WITNESS)) !== 0
   }
 }
 
@@ -18,7 +33,7 @@ const main = async () => {
   const blockRepo = new XyoMemoryBlockRepository()
   const state = new XyoOriginState(stateRepo)
   const hasher = new XyoSha256()
-  const successListener = new XyoBoundWitnessInserter(hasher, state, blockRepo)
+  const originChainInserter = new XyoBoundWitnessInserter(hasher, state, blockRepo)
   const payloadProvider = new XyoOriginPayloadConstructor(state)
   const handler = new XyoZigZagBoundWitnessHander(payloadProvider)
 
@@ -27,18 +42,18 @@ const main = async () => {
   if (state.getIndexAsNumber() === 0) {
     const genesisBlock =  await XyoGenesisBlockCreator.create(state.getSigners(), payloadProvider)
     console.log(`Created genesis block with hash: ${genesisBlock.getHash(hasher).getAll().getContentsCopy().toString('hex')}`)
-    successListener.insert(genesisBlock)
+    originChainInserter.insert(genesisBlock)
   }
 
   tcpNetwork.onPipeCreated = async (pipe) => {
     console.log('New request!')
     try {
       const networkHandle = new XyoNetworkHandler(pipe)
-      const boundWitness = await handler.boundWitness(networkHandle, testProcedureCatalogue, state.getSigners())
+      const boundWitness = await handler.boundWitness(networkHandle, archivistProcedureCatalogue, state.getSigners())
 
       if (boundWitness) {
         console.log(`Created bound witness with hash: ${boundWitness.getHash(hasher).getAll().getContentsCopy().toString('hex')}`)
-        successListener.insert(boundWitness)
+        originChainInserter.insert(boundWitness)
       }
     } catch (error) {
       console.log(`Error creating bound witness: ${error}`)
