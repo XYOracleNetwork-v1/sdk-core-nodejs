3a94917080d2650a476877204c4029151dd8d9ec
Carter Harrison <carterjharrison@gmail.com>
2019-05-10 09:20:38 -0700

    add getter setter interfaces

--- src/persist/xyo-origin-block-repository.ts
+++ src/persist/xyo-origin-block-repository.ts
@@ -3,6 +3,15 @@ export interface IXyoOriginBlockRepository {
   removeOriginBlock(hash: Buffer): Promise<void>
   addOriginBlock(hash: Buffer, block: Buffer): Promise<void>
   addOriginBlocks(hashes: Buffer, blocks: Buffer): Promise<void>
+}
+
+export interface IXyoOriginBlockGetter {
+  initialize(): Promise<boolean>
+
   getOriginBlock(hash: Buffer): Promise<Buffer | undefined>
   getOriginBlocks(limit?: number, offset?: Buffer): Promise<{items: Buffer[], total: number}>
+}
+
+export interface IXyoBlockByPublicKeyRepository {
+  getOriginBlocksByPublicKey(publicKey: Buffer, cursor: Buffer | undefined, limit: number | undefined): Promise<{items: Buffer[], total: number}>
 }
