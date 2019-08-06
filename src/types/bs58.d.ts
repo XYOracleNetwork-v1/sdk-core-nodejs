baafcad47d8e8c10f5476d1838251a8f964a783f
pllearns <thenewphilliplorenzo@outlook.com>
2019-08-06 13:27:56 -0700

    fix: hotfix string for mod and codacy

--- src/types/bs58.d.ts
+++ src/types/bs58.d.ts
@@ -1,4 +1,4 @@
-declare module 'bs58' {
+declare module "bs58" {
     export function encode(buffer: Buffer): string
     export function  decodeUnsafe(string: string): Buffer | undefined
     export function  decode(string: string): Buffer
